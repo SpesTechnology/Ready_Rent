e4772dec2b99b6c553c15129f7593bcfd30c9cc5
Prysla <74454642+Prysla@users.noreply.github.com>
2022-03-17 12:23:46 -0700

    new

--- lib/properties/property_list_screen.dart
+++ lib/properties/property_list_screen.dart
@@ -14,37 +14,51 @@ class PropertyListScreen extends StatelessWidget {
         iconTheme: const IconThemeData(color:kPrimaryColor),
         backgroundColor: Colors.white,
         elevation: 0,
-        title: const Text("Properties"),
+        title: const Text("Properties",style: TextStyle(color: kPrimaryColor),),
       ),
       body: ListView.builder(
         
         itemCount: propertyList.length,
         itemBuilder: (context, index) {
         Property property = propertyList[index];
-        return Card(
-          child: ListTile(
-            leading:Image.asset(property.picture),
-            title: Text(property.title),
-            subtitle: Text(property.description),
-            trailing: TextButton(onPressed: (){
+        return Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: Card(
+            child: ListTile(
+              leading:Image.asset(property.picture),
+              title: Text(property.title),
+              subtitle: Column(
+                children: [
+                  Text(property.details),
+                  Row(
+                    children: [
+                        Icon(property.icon,size: 15,),
+                        Text(property.description)
+                    ],
+                  )
+                  
+                ],
+              ),
+              trailing: TextButton(onPressed: (){
+                  Navigator.push(
+                  context,
+                   MaterialPageRoute(
+                     builder: (context) => DetailsScreen(property)));
+                },
+                           child: const Text('View detials',
+                  style: TextStyle(
+                  color:Colors.black,
+                  fontSize: 15.0),
+                  ),
+              ),
+              onTap: () {
                 Navigator.push(
-                context,
-                 MaterialPageRoute(
-                   builder: (context) => DetailsScreen(property)));
+                  context,
+                   MaterialPageRoute(
+                     builder: (context) => DetailsScreen(property)));
               },
-                         child: const Text('View detials',
-                style: TextStyle(
-                color:Colors.black,
-                fontSize: 15.0),
-                ),
+            
             ),
-            onTap: () {
-              Navigator.push(
-                context,
-                 MaterialPageRoute(
-                   builder: (context) => DetailsScreen(property)));
-            },
-          
           ),
         );
       },),
